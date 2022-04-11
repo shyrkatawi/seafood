71a85470e3f966d588d681094800ae96407cbd9f
Aliaksandr Sakouski <aliaksandr_sakouski@epam.com>
2022-04-12 00:40:54 +0300

    complete day 1

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.header-slider').slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/slider-prev_up-arrow.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/slider-next_down-arrow.svg" alt=""></button>',
+    vertical:true,
+  });
+});
